683905f253dbe7b6708cb0084bfbff47458e3a72
Mucahit Akbas <akbasmucahit10@gmail.com>
2023-03-05 23:24:22 +0300

    ilkAksam

---
+++ Kdv.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Kdv {
+   public static void main(String[] args) {
+    Scanner ham = new Scanner(System.in);
+
+    System.out.println("lütfen ürünü okutunuz");
+    double hamUcret = ham.nextDouble();
+    double kdvİlkMiktarı = hamUcret*18/100;
+    double kdvİkinciMiktarı = hamUcret*8/100;
+    double kdvliİlkUcret= hamUcret*118/100;
+    double kdvliBuyukUcret= hamUcret*108/100;
+     double kdv = hamUcret>1000?kdvİkinciMiktarı:kdvİlkMiktarı;
+
+    System.out.println("kdv miktarınız  :"+ kdv );
+   // System.out.println("kdvli toplam bcretiniz  :"+kdvliBuyukUcret);
+
+   }
+    
+   }
+
